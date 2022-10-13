1cec3b1ec50f4d79eabef63aa38b02633841fb02
Laputa <Laputa1729@163.com>
2022-10-13 23:17:30 +0800

    first commit.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
+/*
+ * 自定义配置vue-cli脚手架
+ * note: 这个配置输出给webpack使用
+ */
+
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+    transpileDependencies: true,
+    lintOnSave: false, // 关闭语法检查
+    // 示例：配置入口文件
+    // pages: {
+    //     index: {
+    //         entry: 'src/main123.js',
+    //     }
+    // }
+});
