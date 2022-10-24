c75e2a3d8cf930dfab82c66a9ff90c711139b32f
Laputa <Laputa1729@163.com>
2022-10-24 23:16:26 +0800

    路由的query参数。

---
+++ 32_src_路由的query参数/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div class="row">
+            <Banner></Banner>
+        </div>
+        <div class="row">
+            <div class="col-xs-2 col-xs-offset-2">
+                <div class="list-group">
+                    <!--<a class="list-group-item" href="./about.html">About</a>-->
+                    <!--<a class="list-group-item active" href="./home.html">Home</a>-->
+
+                    <router-link class="list-group-item" active-class="active" to="/about">About</router-link>
+                    <router-link class="list-group-item" active-class="active" to="/home">Home</router-link>
+                </div>
+            </div>
+            <div class="col-xs-6">
+                <div class="panel">
+                    <div class="panel-body">
+                        <!-- something -->
+                        <router-view></router-view>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--
+            /pages         目录放【路由组件】
+            /components    目录放【一般组件】
+        -->
+
+    </div>
+</template>
+
+<script>
+import Banner from '@/components/Banner';
+
+export default {
+    name: 'App',
+    components: { Banner }
+}
+</script>
+
