8a9ff8a189bf7f6c5cb055c77e7abd770c3c90f7
Laputa <Laputa1729@163.com>
2022-10-25 23:02:53 +0800

    keep-alive.

---
+++ 38_src_缓存路由组件/router/index.js
@@ -0,0 +1,64 @@
+// 路由配置
+import VueRouter from 'vue-router'
+
+import About from '@/pages/About';
+import Home from '@/pages/Home';
+import News from '@/pages/News';
+import Message from '@/pages/Message';
+import Detail from '@/pages/Detail';
+
+// 创建并导出一个路由器
+export default new VueRouter({
+    routes: [
+        {
+            name: 'guanyu',
+            path: '/about',
+            component: About
+        },
+        {
+            path: '/home',
+            component: Home,
+            children: [
+                {
+                    path: 'news',
+                    component: News
+                },
+                {
+                    name: 'xiaoxi',
+                    path: 'message',
+                    component: Message,
+                    children: [
+                        {
+                            name: 'xiangqing',
+                            path: 'detail',
+                            component: Detail,
+                            // props
+
+                            // 1). 该对象中的有所key-value都会以props的形式传给组件，缺点：死数据
+                            // props: { a: 1, b: 'hello' }
+
+                            // 2). 值为布尔值
+                            //     为true，则把路由占位符接收到的params参数，转换为props的形式传给组件
+                            // props: true
+
+                            // 3). 函数
+                            props: function ($route) {
+                                return {
+                                    id: $route.query.id,
+                                    title: $route.query.title,
+                                    a: 1,
+                                    b: 'hello'
+                                }
+                            }
+
+                            // 补充，连续解构赋值，缺点：语义化不好，不推荐
+                            // props({ params: { id, title } }) {
+                            //     return { id, title }
+                            // }
+                        }
+                    ]
+                }
+            ]
+        },
+    ]
+});
