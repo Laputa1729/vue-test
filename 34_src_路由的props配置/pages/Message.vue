6c9768d306e02cbb21c8dcc5baac1e2b07b54691
Laputa <Laputa1729@163.com>
2022-10-25 21:43:42 +0800

    路由的props。

---
+++ 34_src_路由的props配置/pages/Message.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="item in messageList" :key="item.id">
+                <!-- params参数 -->
+                <!--<router-link :to="`/home/message/detail/${item.id}/${item.title}`">{{ item.title }}</router-link>&nbsp;&nbsp;-->
+
+                <!-- to的对象写法 -->
+                <router-link :to="{
+                    // path: '/home/message/detail',
+                    name: 'xiangqing',  // 配合params参数，就不能用path，要用name
+                    params: {
+                        id: item.id,
+                        title: item.title
+                    }
+                }">
+                    {{ item.title }}
+                </router-link>
+            </li>
+        </ul>
+        <hr>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Message',
+    data() {
+        return {
+            messageList: [
+                { id: '001', title: '消息001' },
+                { id: '002', title: '消息002' },
+                { id: '003', title: '消息003' },
+            ]
+        }
+    },
+}
+</script>
